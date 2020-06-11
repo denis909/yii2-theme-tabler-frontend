ced87921594158f6f3c4fd5074fb1c8d6704b0e9
denis909 <denis909@mail.ru>
2020-06-11 15:00:22 +0300

    n/a

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,18 +1,18 @@
 <?php
 
 Yii::$container->setDefinitions([
-    'frontend\theme\FrontendLayout' => 'denis909\themes\tabler\FrontendLayout',
     'frontend\theme\AccountMenu' => 'denis909\themes\tabler\AccountMenu',
     'frontend\theme\Assets' => 'denis909\themes\tabler\Assets',
     'frontend\theme\Card' => 'denis909\themes\tabler\Card',
     'frontend\theme\CardMenu' => 'denis909\themes\tabler\CardMenu',
-    'frontend\theme\ShoppingCart' => 'denis909\themes\tabler\ShoppingCart',
-    'frontend\theme\ShoppingCartItem' => 'denis909\themes\tabler\ShoppingCartItem',
-    'frontend\theme\ShoppingCartMenu' => 'denis909\themes\tabler\ShoppingCartMenu',
     'frontend\theme\DetailView' => 'denis909\themes\tabler\DetailView',
     'frontend\theme\FooterMenu' => 'denis909\themes\tabler\FooterMenu',
     'frontend\theme\GridView' => 'denis909\themes\tabler\GridView',
     'frontend\theme\Layout' => 'denis909\themes\tabler\Layout',
+    'frontend\theme\MainLayout' => 'denis909\themes\tabler\MainLayout',    
     'frontend\theme\MainMenu' => 'denis909\themes\tabler\MainMenu',
+    'frontend\theme\ShoppingCart' => 'denis909\themes\tabler\ShoppingCart',
+    'frontend\theme\ShoppingCartItem' => 'denis909\themes\tabler\ShoppingCartItem',
+    'frontend\theme\ShoppingCartMenu' => 'denis909\themes\tabler\ShoppingCartMenu',
     'frontend\theme\UserMenu' => 'denis909\themes\tabler\UserMenu'
 ]);
